08725968c747fe05b7e873f1fd2768f4ca081f32
Piyush Amitabh <pamitabh@uoregon.edu>
2023-05-10 13:43:23 -0700

    update

--- generate_mip_simple_oswalk.py
+++ generate_mip_simple_oswalk.py
@@ -13,33 +13,31 @@ import tifffile as tiff
 #ask the user for the inputs
 print('Warning: This code ONLY works with single channel z-stack tiff images. It will give unpredictable results with multiple channels.')
 main_dir = input('Enter the Parent directory where ALL the image stacks are stored: ')
+
 # Batchprocess MIP
 channel_names = ['GFP', 'RFP']
-
 for root, subfolders, filenames in os.walk(main_dir):
     for filename in filenames:
         # print(f'Reading: {filename}')
-        
         filepath = os.path.join(root, filename)
         # print(f'Reading: {filepath}')
-
         filename_list = filename.split('.')
         og_name = filename_list[0] #first of list=name
         ext = filename_list[-1] #last of list=extension
 
         if ext=="tif" or ext=="tiff": #tiff files
-            print(f'Reading Image: {filepath}')
             read_image = tiff.imread(filepath)
 
-            if len(read_image.shape)!=2: #image stacks, create MIP
-                arr_mip = np.max(read_image, axis=0)
+            if len(read_image.shape)==3: #check if 3D images
+                print(f'Processing MIP for: {filepath}')
+                arr_mip = np.max(read_image, axis=0) #create MIP
 
-            for ch_name in channel_names:
-                if ch_name.casefold() in og_name.casefold():
-                    dest = os.path.join(root, ch_name.casefold()+'_mip')
-                    if not os.path.exists(dest): #check if the dest exists
-                        print("Write path doesn't exist.")
-                        os.makedirs(dest)
-                        print(f"Directory '{ch_name.casefold()}'_mip created")
-                img_mip = Image.fromarray(arr_mip)
-                img_mip.save(os.path.join(dest, og_name+'_mip.tif'))
+                for ch_name in channel_names: #save mip array in right directory with correct channel name
+                    if ch_name.casefold() in og_name.casefold():
+                        dest = os.path.join(root, ch_name.casefold()+'_mip')
+                        if not os.path.exists(dest): #check if the dest exists
+                            print("Write path doesn't exist.")
+                            os.makedirs(dest)
+                            print(f"Directory '{ch_name.casefold()}_mip' created")
+                        img_mip = Image.fromarray(arr_mip)
+                        img_mip.save(os.path.join(dest, og_name+'_mip.tif'))

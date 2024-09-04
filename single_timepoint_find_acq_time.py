6da654ffe95f11bb58979d784be1e06d6a8c60ef
pamitabh <pamitabh@uoregon.edu>
2024-09-03 17:05:29 -0700

    working code

--- single_timepoint_find_acq_time.py
+++ single_timepoint_find_acq_time.py
@@ -1,6 +1,5 @@
 import os
 import pathlib
-from copy import deepcopy
 
 import pandas as pd
 from natsort import natsorted
@@ -20,51 +19,16 @@ for root, dirs, files in os.walk(top_dir):
 save_directory_default = os.path.expanduser(os.path.join("~", "Documents"))
 save_directory = input("Enter the Save folder for original images [default-Documents]: ") or save_directory_default
 
-def format_time_df(times):
-    delta_t_df = pd.DataFrame(times)
-    delta_t_df = delta_t_df.reset_index()
-    # Rename colnames
-    col_names = ['bf_snap_acq_start_time', 'gfp_ztack_acq_start_time', 'rfp_ztack_acq_start_time']
-    delta_col_names = [f'delta_({col})_s' for col in col_names]
-    delta_t_df = delta_t_df.rename(columns={'index': 'time_point',
-                                            'BF snap': col_names[0],
-                                            'GFP zstack': col_names[1],
-                                            'RFP zstack': col_names[2]})
-    #parse to get time_point in integer
-    delta_t_df['time_point'] = delta_t_df['time_point'].str.extract(r'(\d+)').astype(int)
-
-    for col_name, del_col_name in zip(col_names, delta_col_names) : #run for all columns except the time_point
-        delta_t_df[col_name] = pd.to_datetime(delta_t_df[col_name]) #str to datetime objects
-        delta_t_df[del_col_name] = (pd.to_datetime(delta_t_df[col_name])).diff().dt.total_seconds() #del t
-        # only keep the time, drop the year
-        # delta_t_df[col] = delta_t_df[col].dt.time
-    mean_values = delta_t_df[delta_col_names].mean()
-    # print(mean_values)
-    std_values = delta_t_df[delta_col_names].std()
-    # print(std_values)
-    coe_df = (std_values/mean_values) * 100
-    if len(coe_df[coe_df>5])>0: #give error if coe more than 5%
-        print("ERROR:: Bad acquisition time interval (coe > 5%). Listed below..")
-        print('Mean values:')
-        print(mean_values[coe_df>5])
-        print('STD values:')
-        print(std_values[coe_df>5])
-    else:
-        print("**Good** acquisition time interval (coe < 5%)..")
-        print('Mean values:')
-        print(mean_values)
-    return(delta_t_df)
-
-save_directory = pathlib.Path(save_directory).joinpath(f"{top_dir.parent.name}_delta_t")
+save_directory = pathlib.Path(save_directory).joinpath(f"{pathlib.Path(top_dir).name}_delta_t")
 save_directory.mkdir(parents=True, exist_ok=True)
-delta_t_per_fish_list = []
-delta_t_df_concat = pd.DataFrame()
 
+df_per_fish_list = []
 for acq_folder_path in acq_folder_path_list:
     acq_folder = pathlib.Path(acq_folder_path)
 
     for fish in natsorted([dir for dir in acq_folder.iterdir() if dir.is_dir()]):
-        times = {}
+        # times = {}
+        tp_list, acq_folder_path_list, fish_list, channel_list, imaging_list, pos_list = [], [], [], [], [], []
         fish_folder = acq_folder.joinpath(fish)
         print(f"Processing {fish_folder}..")
         for pos in natsorted([dir for dir in fish_folder.iterdir() if dir.is_dir()]):
@@ -89,11 +53,25 @@ for acq_folder_path in acq_folder_path_list:
                                 except KeyError:
                                     pass
                         channel_time[tp.name] = time_received
-                    times[f"{acq_folder_path} {fish} {channel.name} {imaging.name}"] = deepcopy(channel_time)
+                    #get time
+                    tp_list.append(channel_time)
+                    #get other info
+                    acq_folder_path_list.append(acq_folder_path)
+                    fish_list.append(fish.name)
+                    pos_list.append(pos.name)
+                    imaging_list.append(imaging.name)
+                    channel_list.append(channel.name)
         
         # all_acq_times.append(times)
-        delta_t_df = format_time_df(times)
-        delta_t_per_fish_list.append(delta_t_df)
-        
-    delta_t_df_concat = pd.concat([delta_t_df_concat, delta_t_df], axis=0, ignore_index=True)
-delta_t_df_concat.to_csv(save_directory.joinpath(f"delta_t.csv"))
+        df_one_fish = pd.DataFrame({'acquisition_start_time': tp_list, 
+                                    'acquisition_path': acq_folder_path_list,
+                                    'fish': fish_list,
+                                    'pos': pos_list,
+                                    'imaging': imaging_list,
+                                    'channel': channel_list
+                                    })
+        # delta_t_df = format_time_df(times)
+        df_per_fish_list.append(df_one_fish)
+
+all_df = pd.concat(df_per_fish_list, ignore_index=True)
+all_df.to_csv(save_directory.joinpath("delta_t.csv"))

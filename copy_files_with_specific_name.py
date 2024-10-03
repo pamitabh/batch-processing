af0c7795851ef3529e805df5607b436e240168bb
pamitabh <pamitabh@uoregon.edu>
2024-10-03 11:50:55 -0700

    update to include pathname specification

--- copy_files_with_specific_name.py
+++ copy_files_with_specific_name.py
@@ -2,10 +2,19 @@ import os
 import shutil
 
 
-def copy_mask_files(source, target, file_substring):
+def copy_files(source, target, filename_substring_include, pathname_substring_include):
+    
     for root, dirs, files in os.walk(source):
         for file in files:
-            if file_substring in file.casefold():
+            if filename_substring_include == "":
+                string_comparison = pathname_substring_include.casefold() in root.casefold()
+            if pathname_substring_include == "":
+                string_comparison = filename_substring_include.casefold() in file.casefold()
+            if (filename_substring_include != "") and (pathname_substring_include != ""):
+                string_comparison = (filename_substring_include.casefold() in file.casefold()) and (pathname_substring_include.casefold() in root.casefold())
+                # print(file, root, string_comparison)
+                
+            if string_comparison:
                 # Construct full file path
                 source_file = os.path.join(root, file)
 
@@ -27,11 +36,15 @@ def copy_mask_files(source, target, file_substring):
 # Ask user for source and target directories
 source_directory = input("Enter the source directory: ")
 target_directory = input("Enter the target directory: ")
-file_substring = input(
-    "Enter the substring to filter filenames for copying (can have extensions): "
+file_substring_include = input(
+    "Enter the substring of filenames to INCLUDE for copying (can have extensions): "
 )
 
-copy_mask_files(source_directory, target_directory, file_substring=file_substring)
+pathname_substring_include = input(
+    "Enter the substring of pathname to INCLUDE for copying [default='']: "
+) or ""
+
+copy_files(source_directory, target_directory, filename_substring_include=file_substring_include, pathname_substring_include=pathname_substring_include)
 
 # add successful message
 print(f"Done! Copied files are in: {target_directory}")
